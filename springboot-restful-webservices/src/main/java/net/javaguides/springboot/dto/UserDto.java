fcc54f967408f852c13b07ff50a64086b1a83d65
Minal2598 <minalpatil9825@gmail.com>
2025-02-18 23:13:58 +0530

    Add Validation concept

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,18 @@ import lombok.Setter;
 public class UserDto {
 
     private Long userId;
+
+    // Client has requirement -> User first Name should not be null or empty
+    @NotEmpty(message = "User first Name should not be null or empty")
     private String firstName;
+
+    // Client has requirement -> User Last Name should not be null or empty
+    @NotEmpty(message = "User Last Name should not be null or empty")
     private String lastName;
+
+    // Client has requirement -> Email Address should not be null or empty
+    // Email Address should be valid
+    @NotEmpty(message = " Email Address should not be null or empty")
+    @Email(message = "Email Address should be valid")
     private String email;
 }
